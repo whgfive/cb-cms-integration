84b2ac5b38f86bc93e82a2d1c625d8e19fbeda07
William Garrison <wgarrison@salesforce.com>
2018-05-10 13:34:23 -0700

    whatever

--- icon.php
+++ icon.php
@@ -1,9 +1,9 @@
 <?php
 $cms_icon = getenv('cms_icon');
-$fp = fopen($name, 'rb');
+/*$fp = fopen($name, 'rb');
 
 header("Content-Type: image/png");
-header("Content-Length: " . filesize($name));
+header("Content-Length: " . filesize($name));*/
 
 echo $cms_icon;
 ?>

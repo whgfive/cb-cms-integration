ac43ca6f8511e6129252a60120ee7059453b0a5c
William Garrison <wgarrison@salesforce.com>
2018-05-10 13:39:36 -0700

    whatever

--- icon.php
+++ icon.php
@@ -1,18 +1,9 @@
 <?php
 $cms_icon = getenv('cms_icon');
-$ch = curl_init($cms_icon);    
+$fp = fopen($cms_icon, 'rb');
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+header("Content-Type: image/png");
+header("Content-Length: " . filesize($fp));*/
 
-$data = curl_exec($ch);
-curl_close($ch);
-
-if ($data === false) {
-        die('cURL failed');
-}
-
-header('Content-Type: image/png');
-header('Content-Length: ' . curl_getinfo( $ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD ) );
-echo $data;
+echo $fp;
 ?>

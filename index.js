584ccc63cb90fe33a9fc41ac7d49b9431d582e50
William Garrison <wgarrison@salesforce.com>
2018-05-11 14:44:06 -0700

    whatever

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const request = require('request')
-//const sharp = require('sharp');
+const sharp = require('sharp');
 
 const PORT = process.env.PORT || 5000
 var app = express();

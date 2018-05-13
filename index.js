da3edee02c621d6c9df9b765370670b040ceb71e
William Garrison <wgarrison@salesforce.com>
2018-05-13 01:18:27 -0700

    whatever

--- index.js
+++ index.js
@@ -2,28 +2,33 @@ const express = require('express')
 const path = require('path')
 const request = require('request')
 const sharp = require('sharp');
-var fs = require('fs');
+const fs = require('fs');
+const lodash = require('lodash');
 
 const PORT = process.env.PORT || 5000
 var app = express();
 
 
 var cms_icon,cms_logo,cms_name = "";
+var cms_images,cms_partners_json,cms_content=[];
+var cms_brand = process.env.cms_brand || 'nto';
+
 fs.readFile('cms-partners.json',
 	function(err, data) {		
 		var jsonData = data;
 		var jsonParsed = JSON.parse(jsonData);
-		cms_icon = process.env.cms_icon || jsonParsed.sitecore.cms_icon;
-		cms_logo = process.env.cms_logo || jsonParsed.sitecore.cms_logo_url;
-		cms_name = process.env.cms_name || jsonParsed.sitecore.cms_name;
+		cms_partners_json = jsonParsed;
+		cms_icon = process.env.cms_icon || jsonParsed[0].cms_icon;
+		cms_logo = process.env.cms_logo || jsonParsed[0].cms_logo_url;
+		cms_name = process.env.cms_name || jsonParsed[0].cms_name;
 });
 
-var cms_images=[];
 fs.readFile('cb-content.json',
 	function(err, data) {		
 		var jsonData = data;
 		var jsonParsed = JSON.parse(jsonData);
-		cms_images = jsonParsed.travel.img_src;
+		cms_content = jsonParsed;
+		cms_images = lodash.filter(jsonParsed, { 'id': cms_brand })[0].img_src;
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -37,6 +42,13 @@ app.get('/', function(req, res){
 		"cms_logo":cms_logo,
 		"cms_name":cms_name,
 		"cms_images":cms_images
+	});
+}); 
+
+app.get('/install', function(req, res){
+	res.render('pages/install', {
+		"cms_content":cms_content,
+		"cms_partners":cms_partners_json
 	});
 }); 
 

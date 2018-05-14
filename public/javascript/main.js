0dadfb23b5d8f0ebfd42c8c3f1d423e15461bd77
William Garrison <wgarrison@salesforce.com>
2018-05-13 22:52:17 -0700

    whatever

---
+++ public/javascript/main.js
@@ -0,0 +1,21 @@
+var selectedImageId, image;
+
+function chosen(imgId) {
+    var sdk = new window.sfdc.BlockSDK();
+    sdk.setContent("");
+    var fit = document.getElementById("scaleFit").checked;
+    var acenter = document.getElementById("alignCenter").checked;
+    selectedImageId = imgId;
+    if (imgId != "") {
+        image = document.getElementById(imgId).src;
+    }
+    if (fit) {
+        sdk.setContent("<img width='100%' src='" + image + "'/>");
+    } else {
+        if (acenter) {
+            sdk.setContent("<center><img src='" + image + "'/></center>");
+        } else {
+            sdk.setContent("<img src='" + image + "'/>");
+        }
+    }
+}

ea5eb38dbac9a388144e196c0b7857136f706d16
William Garrison <wgarrison@salesforce.com>
2018-05-13 23:17:12 -0700

    whatever

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,11 +1,9 @@
-var selectedImageId, image;
-
 function chosen(imgId) {
     var sdk = new window.sfdc.BlockSDK();
     sdk.setContent("");
     var fit = document.getElementById("scaleFit").checked;
     var acenter = document.getElementById("alignCenter").checked;
-    selectedImageId = imgId;
+    //selectedImageId = imgId;
     if (imgId != "") {
         image = document.getElementById(imgId).src;
     }
